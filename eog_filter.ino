708ed89ffcb26ad9b0252281d9bbcc20d2613400
Robotics and Instrumentation Society of IISER Mohali <roboticsclub@iisermohali.ac.in>
2024-09-22 15:16:21 +0530

    eog_filter.ino

--- eog_filter.ino
+++ eog_filter.ino
@@ -25,17 +25,20 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#define SAMPLE_RATE 75
+#define SAMPLE_RATE 50
 #define BAUD_RATE 115200
 #define INPUT_PIN A0
-
+#define INPUT_PIN1 A1
+char direction = 'S';
 
 void setup() {
   // Serial connection begin
   Serial.begin(BAUD_RATE);
+  
 }
 
 void loop() {
+   
   // Calculate elapsed time
   static unsigned long past = 0;
   unsigned long present = micros();
@@ -49,9 +52,13 @@ void loop() {
   // Sample
   if(timer < 0){
     timer += 3000000 / SAMPLE_RATE;
-    float sensor_value = analogRead(INPUT_PIN);
-    float signal = EOGFilter(sensor_value);
-    Serial.println(signal);
+    float sensor_value1 = analogRead(INPUT_PIN);
+    float sensor_value2 = analogRead(INPUT_PIN1);
+    float signal1 = EOGFilter1(sensor_value1);
+    float signal2 = EOGFilter2(sensor_value2);
+    Serial.println(signal1);
+    Serial.println(signal2);
+    Serial.println(direction);
   }
 }
 
@@ -61,7 +68,7 @@ void loop() {
 // Reference: 
 // https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.butter.html
 // https://courses.ideate.cmu.edu/16-223/f2020/Arduino/FilterDemos/filter_gen.py
-float EOGFilter(float input)
+float EOGFilter1(float input)
 {
   float output = input;
   {
@@ -95,6 +102,62 @@ float EOGFilter(float input)
     if(output>=-150 and output<=150){
       output = 0;
       }
+    if(output<=-500 and output>=500){
+      direction = 'S';
+    }
+    if (output> 150){
+     direction ='F';
+      }
+    if (output<-150){
+      direction ='B';
+      }
   
   return output;
 }
+float EOGFilter2(float input)
+{
+  float output = input;
+  {
+    static float z1, z2; // filter section state
+    float x = output - 0.02977423*z1 - 0.04296318*z2;
+    output = 0.09797471*x + 0.19594942*z1 + 0.09797471*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - 0.08383952*z1 - 0.46067709*z2;
+    output = 1.00000000*x + 2.00000000*z1 + 1.00000000*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - -1.92167271*z1 - 0.92347975*z2;
+    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - -1.96758891*z1 - 0.96933514*z2;
+    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
+    z2 = z1;
+    z1 = x;
+  }
+    if(output>=-150 and output<=150){
+      output = 0;
+      }
+    if(output<=-500 and output>=500){
+      direction = 'S';
+    }
+    if (output> 150){
+     direction ='L';
+      }
+    if (output<-150){
+      direction ='R';
+      }
+  
+  return output;
+}
+
