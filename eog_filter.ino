5b58c0e4db0a6f266d0d5c83567079c68d764597
Robotics and Instrumentation Society of IISER Mohali <roboticsclub@iisermohali.ac.in>
2024-09-26 01:29:52 +0530

    eog_filter.ino
    eog filter with direction output and bluetooth communication

--- eog_filter.ino
+++ eog_filter.ino
@@ -1,44 +1,24 @@
 // EOG Filter - BioAmp EXG Pill
 // https://github.com/upsidedownlabs/BioAmp-EXG-Pill
 
-// Upside Down Labs invests time and resources providing this open source code,
-// please support Upside Down Labs and open-source hardware by purchasing
-// products from Upside Down Labs!
-
-// Copyright (c) 2021 Upside Down Labs - contact@upsidedownlabs.tech
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
 #define SAMPLE_RATE 50
-#define BAUD_RATE 115200
 #define INPUT_PIN A0
 #define INPUT_PIN1 A1
 char direction = 'S';
+char lastDirection = 'S'; // To track the last direction sent
+
+#include <SoftwareSerial.h>
+#include <Wire.h>
+
+SoftwareSerial BTSerial(2,3);
 
 void setup() {
   // Serial connection begin
-  Serial.begin(BAUD_RATE);
-  
+  Serial.begin(9600);
+  BTSerial.begin(9600);
 }
 
 void loop() {
-   
   // Calculate elapsed time
   static unsigned long past = 0;
   unsigned long present = micros();
@@ -50,114 +30,129 @@ void loop() {
   timer -= interval;
 
   // Sample
-  if(timer < 0){
+  if (timer < 0) {
     timer += 3000000 / SAMPLE_RATE;
     float sensor_value1 = analogRead(INPUT_PIN);
     float sensor_value2 = analogRead(INPUT_PIN1);
     float signal1 = EOGFilter1(sensor_value1);
     float signal2 = EOGFilter2(sensor_value2);
-    Serial.println(signal1);
+
+    // Print filtered signals for debugging
+    Serial.print("Signal1:");
+    Serial.print(signal1);
+    Serial.print(',');
+    Serial.print("Signal2:");
     Serial.println(signal2);
-    Serial.println(direction);
+    //Serial.println(direction);
+   // BTSerial.write(direction);
+     //Send direction only if it has changed
+    if (direction != lastDirection) {
+      BTSerial.write(direction);
+      Serial.println(direction);
+      lastDirection = direction; // Update last direction sent
+    }
   }
 }
 
-// Band-Pass Butterworth IIR digital filter, generated using filter_gen.py.
-// Sampling rate: 75.0 Hz, frequency: [0.5, 19.5] Hz.
-// Filter is order 4, implemented as second-order sections (biquads).
-// Reference: 
-// https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.butter.html
-// https://courses.ideate.cmu.edu/16-223/f2020/Arduino/FilterDemos/filter_gen.py
-float EOGFilter1(float input)
-{
+// Band-Pass Butterworth IIR digital filter for Sensor 1
+float EOGFilter1(float input) {
   float output = input;
   {
     static float z1, z2; // filter section state
-    float x = output - 0.02977423*z1 - 0.04296318*z2;
-    output = 0.09797471*x + 0.19594942*z1 + 0.09797471*z2;
+    float x = output - 0.02977423 * z1 - 0.04296318 * z2;
+    output = 0.09797471 * x + 0.19594942 * z1 + 0.09797471 * z2;
     z2 = z1;
     z1 = x;
   }
   {
     static float z1, z2; // filter section state
-    float x = output - 0.08383952*z1 - 0.46067709*z2;
-    output = 1.00000000*x + 2.00000000*z1 + 1.00000000*z2;
+    float x = output - 0.08383952 * z1 - 0.46067709 * z2;
+    output = 1.00000000 * x + 2.00000000 * z1 + 1.00000000 * z2;
     z2 = z1;
     z1 = x;
   }
   {
     static float z1, z2; // filter section state
-    float x = output - -1.92167271*z1 - 0.92347975*z2;
-    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
+    float x = output - -1.92167271 * z1 - 0.92347975 * z2;
+    output = 1.00000000 * x + -2.00000000 * z1 + 1.00000000 * z2;
     z2 = z1;
     z1 = x;
   }
   {
     static float z1, z2; // filter section state
-    float x = output - -1.96758891*z1 - 0.96933514*z2;
-    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
+    float x = output - -1.96758891 * z1 - 0.96933514 * z2;
+    output = 1.00000000 * x + -2.00000000 * z1 + 1.00000000 * z2;
     z2 = z1;
     z1 = x;
   }
-    if(output>=-150 and output<=150){
-      output = 0;
-      }
-    if(output<=-500 and output>=500){
-      direction = 'S';
-    }
-    if (output> 150){
-     direction ='F';
-      }
-    if (output<-150){
-      direction ='B';
-      }
-  
-  return output;
-}
-float EOGFilter2(float input)
-{
-  float output = input;
-  {
-    static float z1, z2; // filter section state
-    float x = output - 0.02977423*z1 - 0.04296318*z2;
-    output = 0.09797471*x + 0.19594942*z1 + 0.09797471*z2;
-    z2 = z1;
-    z1 = x;
+
+  // Adjust output and direction based on the filtered signal
+  if (output >= -150 && output <= 150) {
+    output = 0;
   }
-  {
-    static float z1, z2; // filter section state
-    float x = output - 0.08383952*z1 - 0.46067709*z2;
-    output = 1.00000000*x + 2.00000000*z1 + 1.00000000*z2;
-    z2 = z1;
-    z1 = x;
+
+  // Check for each condition independently
+  else if (output > 500 || output < -500) {
+    direction = 'S';  // Still when output exceeds 500 or falls below -500
   }
-  {
-    static float z1, z2; // filter section state
-    float x = output - -1.92167271*z1 - 0.92347975*z2;
-    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
-    z2 = z1;
-    z1 = x;
+  else if (output > 150) {
+    direction = 'F';  // Forward
   }
-  {
-    static float z1, z2; // filter section state
-    float x = output - -1.96758891*z1 - 0.96933514*z2;
-    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
-    z2 = z1;
-    z1 = x;
+  else if (output < -150) {
+    direction = 'B';  // Backward
   }
-    if(output>=-150 and output<=150){
-      output = 0;
-      }
-    if(output<=-500 and output>=500){
-      direction = 'S';
-    }
-    if (output> 150){
-     direction ='L';
-      }
-    if (output<-150){
-      direction ='R';
-      }
-  
+
   return output;
 }
 
+// Band-Pass Butterworth IIR digital filter for Sensor 2
+float EOGFilter2(float input) {
+  float output = input;
+  {
+    static float z1, z2; // filter section state
+    float x = output - 0.02977423 * z1 - 0.04296318 * z2;
+    output = 0.09797471 * x + 0.19594942 * z1 + 0.09797471 * z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - 0.08383952 * z1 - 0.46067709 * z2;
+    output = 1.00000000 * x + 2.00000000 * z1 + 1.00000000 * z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - -1.92167271 * z1 - 0.92347975 * z2;
+    output = 1.00000000 * x + -2.00000000 * z1 + 1.00000000 * z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - -1.96758891 * z1 - 0.96933514 * z2;
+    output = 1.00000000 * x + -2.00000000 * z1 + 1.00000000 * z2;
+    z2 = z1;
+    z1 = x;
+  }
+
+  // Adjust output and direction based on the filtered signal
+  if (output >= -150 && output <= 150) {
+    output = 0;
+    
+  }
+
+  // Check for each condition independently
+  else if (output > 500 || output < -500) {
+    direction = 'S';  // Still when output exceeds 500 or falls below -500
+  }
+ else if (output > 150) {
+    direction = 'L';  // Left
+  }
+ else if (output < -150) {
+    direction = 'R';  // Right
+  }
+
+  return output;
+}
